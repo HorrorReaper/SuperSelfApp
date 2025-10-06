b07e1e329e224979c28901c8153a8be97b3f0dff
HorrorReaper <patrick.egi04@gmail.com>
2025-10-06 08:12:51 +0200

    Hubs auf Dashboard

--- superself/app/dashboard/page.tsx
+++ superself/app/dashboard/page.tsx
@@ -8,6 +8,7 @@ import JourneyCard from "@/components/dashboard/JourneyCard";
 import { AchievementUnlockToaster } from "@/components/achievements/unlock-toaster";
 import { Button } from "@/components/ui/button";
 import StartNewJourneyModal from '@/components/dashboard/StartNewJourneyModal';
+import { HubsDashboard } from "@/components/dashboard/HubsDashboard";
 
 export default function DashboardPage() {
     const [user, setUser] = useState<User | null>(null);
@@ -110,6 +111,7 @@ export default function DashboardPage() {
                     </div>
 
                     <div className="mt-10 space-y-4 flex flex-col items-center mb-10">
+                        <HubsDashboard />
                         <JourneyCard userid={user?.id} />
                         <StartNewJourneyModal userid={user?.id}>
                             <Button className="text-center hover:cursor-pointer">Start New Journey</Button>
